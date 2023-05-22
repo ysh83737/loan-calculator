1cf0b75eb9a57adac5ef1a235d10e9ae0ace089d
Shawn Yuan <ysh83737@hotmail.com>
2023-05-22 22:07:02 +0800

    chore: dep element-plus and import automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [/\.[tj]s?$/, /\.vue$/],
+      imports: ['vue'],
+      resolvers: [ElementPlusResolver()],
+      dts: './src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+        filepath: 'src/.eslintrc-auto-import.json',
+        globalsPropValue: true
+      }
+    }),
+    Components({
+      dirs: ['src/components'],
+      resolvers: [
+        ElementPlusResolver()
+      ],
+      dts: './src/components.d.ts'
+    })
+  ]
 })
